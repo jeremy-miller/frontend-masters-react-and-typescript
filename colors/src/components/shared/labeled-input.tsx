0d0257deb9833f41cfa6fdda289639e387bfc74f
Jeremy Miller <jeremymiller@fastmail.com>
2025-01-11 16:12:41 -0800

    Customize labeled input to be more generic

--- colors/src/components/shared/labeled-input.tsx
+++ colors/src/components/shared/labeled-input.tsx
@@ -1,36 +1,16 @@
-import clsx from 'clsx';
-import { ChangeEventHandler, useId } from 'react';
+import { ComponentPropsWithoutRef, useId } from 'react';
 
-type LabeledInputProps = {
-  id?: string;
+type LabeledInputProps = ComponentPropsWithoutRef<'input'> & {
   label: string;
-  value: string | number;
-  type?: string;
-  className?: string;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
 };
 
-const LabeledInput = ({
-  label,
-  value,
-  id,
-  className,
-  type = 'text',
-  onChange,
-}: LabeledInputProps) => {
-  id = useId() + id;
+const LabeledInput = ({ label, ...props }: LabeledInputProps) => {
+  const id = useId();
 
   return (
     <div>
       <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        value={value}
-        onChange={onChange}
-        type={type}
-        className={clsx('w-full', className)}
-        readOnly={!onChange}
-      />
+      <input id={id} {...props} />
     </div>
   );
 };
